44970c24bb5356db2ba9fc294d1b9b37c6cb2aa3
Applederp666 <112904109+Applederp666@users.noreply.github.com>
2024-02-25 10:39:25 -0600

    Add files via upload

---
+++ Compiler.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+enum Classification {
+    Letter,
+    Digit,
+    ReservedWord,
+    Operator,
+    Whitespace,
+    Error,
+    Delimiters,
+    Pren,
+    Braces,
+    Integer,
+    Variable
+};
+ 
+int main() {
+    return 0;
+}
